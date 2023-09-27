f95e3120ed59d034cde19a1abc3d895a44feb6dc
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-27 23:48:36 +0000

    update all tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@
 file_line{'Set alias name':
   path => '/etc/ssh/ssh_config',
   line => 'aplus_server
-    HostName 34.204.81.175
+    HostName 54.158.207.18
     User ubuntu
     IdentityFile ~/.ssh/school
     PasswordAuthentication no',
